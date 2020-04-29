9b7429aecb58c9e35fbeb608a4dfea7c791bda81
Maryam <Maryam_moein@hotmail.com>
2020-04-29 23:22:19 +0800

    implement form validation on register user form

--- TodoList/Controller/RegisterViewController.swift
+++ TodoList/Controller/RegisterViewController.swift
@@ -17,13 +17,23 @@ class RegisterViewController: UIViewController {
     @IBOutlet weak var contactNoTxt: UITextField!
     @IBOutlet weak var userNameTxt: UITextField!
     @IBOutlet weak var passwordTxt: UITextField!
+    @IBOutlet weak var errorLbl: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        errorLbl.isHidden = true
     }
         
     @IBAction func save(_ sender: Any) {
+        
+        errorLbl.isHidden = true
+        
+        validateFirstName()
+        validateLastName()
+        validateContactNo()
+        validateUserName()
+        validatePassword()
+
         let url = "http://192.168.1.5:8080/api/user/register"
 
         let json = [
@@ -44,5 +54,45 @@ class RegisterViewController: UIViewController {
                     }
                 }
         }
+    
+    func validateFirstName() {
+        guard let firstName = firstNameTxt.text, firstNameTxt.text?.count != 0 else {
+            errorLbl.isHidden = false
+            errorLbl.text = "Please enter the firstName"
+            return
+        }
+    }
+    
+    func validateLastName() {
+        guard let lastName = lastNameTxt.text, lastNameTxt.text?.count != 0 else {
+            errorLbl.isHidden = false
+            errorLbl.text = "Please enter the lastName"
+            return
+        }
+    }
+    
+    func validateContactNo() {
+        guard let contactNo = contactNoTxt.text, contactNoTxt.text?.count != 0 else {
+            errorLbl.isHidden = false
+            errorLbl.text = "Please enter the contactNo"
+            return
+        }
+    }
+    
+    func validateUserName() {
+        guard let userName = userNameTxt.text, userNameTxt.text?.count != 0 else {
+            errorLbl.isHidden = false
+            errorLbl.text = "Please enter the userName"
+            return
+        }
+    }
+    
+    func validatePassword() {
+        guard let password = passwordTxt.text, passwordTxt.text?.count != 0 else {
+            errorLbl.isHidden = false
+            errorLbl.text = "Please enter the password"
+            return
+        }
+    }
     
 }
